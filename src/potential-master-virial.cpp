6b476ff1f84f2848a025ce698ef24a9c9b75276c
Andrew Schultz <ajs42@buffalo.edu>
2018-03-01 09:06:52 -0500

    Add implementation of PotentialMaster for virial calculations

---
+++ src/potential-master-virial.cpp
@@ -0,0 +1,43 @@
+#include "potential-master.h"
+
+PotentialMasterVirial::PotentialMasterVirial(const SpeciesList &sl, Box &box) : PotentialMaster(sl,box) {}
+
+void PotentialMasterVirial::computeMolecules(const int* iMoleculeList, const int nMolecules, double &energy) {
+  double dr[3];
+  /*uAtomsChangedSet.clear();
+  for (int i=0; i<numAtoms; i++) uAtomsChangedSet.insert(i);
+  duAtom.resize(numAtoms);
+  fill(duAtom.begin(), duAtom.end(), 0.0);*/
+  for (int i=0; i<nMolecules-1; i++) {
+    int iMolecule = iMoleculeList[i];
+    int iSpecies, iFirstAtom, iLastAtom;
+    box.getMoleculeInfo(iMolecule, iSpecies, iFirstAtom, iLastAtom);
+    for (int iAtom=iFirstAtom; iAtom<=iLastAtom; iAtom++) {
+      int iType = box.getAtomType(iAtom);
+      double* iCutoffs = pairCutoffs[iType];
+      Potential** iPotentials = pairPotentials[iType];
+      double *ri = box.getAtomPosition(iAtom);
+
+      for (int j=i+1; j<nMolecules; j++) {
+        int jMolecule = iMoleculeList[j];
+        int jSpecies, jFirstAtom, jLastAtom;
+        box.getMoleculeInfo(jMolecule, jSpecies, jFirstAtom, jLastAtom);
+        for (int jAtom=jFirstAtom; jAtom<=jLastAtom; jAtom++) {
+          //uAtomsChangedSet.insert(iAtom);
+          int jType = box.getAtomType(jAtom);
+          double *rj = box.getAtomPosition(jAtom);
+          for (int k=0; k<3; k++) dr[k] = rj[k]-ri[k];
+          box.nearestImage(dr);
+          double r2 = 0;
+          for (int k=0; k<3; k++) r2 += dr[k]*dr[k];
+          if (r2 > iCutoffs[jType]) continue;
+          //uAtomsChangedSet.insert(jAtom);
+          double uij = iPotentials[jType]->u(r2);
+          //duAtom[jAtom] += 0.5*uij;
+          //duAtom[iAtom] += 0.5*uij;
+          energy += uij;
+        }
+      }
+    }
+  }
+}
