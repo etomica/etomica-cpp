a6e23eb68a1ec2550227b48e38c864870e50ccf9
Andrew Schultz <ajs42@buffalo.edu>
2018-03-18 20:32:19 -0400

    Fix printing of various correlations, add correlation between full avgs

--- src/virial-production.cpp
+++ src/virial-production.cpp
@@ -1,10 +1,11 @@
+#include <string.h>
 #include <cmath>
 #include "virial.h"
 #include "alloc2d.h"
 
 // perhaps just take Clusters and make Meter and Average internally
 
-VirialProduction::VirialProduction(IntegratorMC &rIntegrator, IntegratorMC &tIntegrator, Cluster &refClusterRef, Cluster &refClusterTarget, Cluster &targetClusterRef, Cluster &targetClusterTarget, double alpha, double ri) : refIntegrator(rIntegrator), targetIntegrator(tIntegrator), refMeter(MeterVirialOverlap(refClusterRef, refClusterTarget, alpha, 0, 1)), targetMeter(MeterVirialOverlap(targetClusterTarget, targetClusterRef, 1/alpha, 0, 1)), refAverage(2, 1, 1000, true), targetAverage(targetClusterTarget.numValues()+1, 1, 1000, true), refPump(refMeter,1,&refAverage), targetPump(targetMeter,1,&targetAverage), idealTargetFraction(0.5), refIntegral(ri), refSteps(0), targetSteps(0) {
+VirialProduction::VirialProduction(IntegratorMC &rIntegrator, IntegratorMC &tIntegrator, Cluster &refClusterRef, Cluster &refClusterTarget, Cluster &targetClusterRef, Cluster &targetClusterTarget, double alpha, double ri) : refIntegrator(rIntegrator), targetIntegrator(tIntegrator), refMeter(MeterVirialOverlap(refClusterRef, refClusterTarget, alpha, 0, 1)), targetMeter(MeterVirialOverlap(targetClusterTarget, targetClusterRef, 1/alpha, 0, 1)), refAverage(2, 1, 1000, true), targetAverage(targetClusterTarget.numValues()+1, 1, 1000, true), refPump(refMeter,1,&refAverage), targetPump(targetMeter,1,&targetAverage), idealTargetFraction(0.5), refIntegral(ri), disposed(false), refSteps(0), targetSteps(0) {
   int numTargets = targetAverage.getNumData();
   fullStats = (double**)malloc2D(numTargets, 2, sizeof(double));
   refIntegrator.addListener(&refPump);
@@ -75,17 +76,51 @@ void VirialProduction::printResults(const char **targetNames) {
   printf("reference ratio:           % 22.15e  error: %12.5e   cor: % 7.5f\n", refRatioStats[0][AVG_AVG], refRatioStats[0][AVG_ERR], refBCStats[0][1]);
   printf("reference average:         % 22.15e  error: %12.5e  acor: % 7.5f\n", refStats[0][AVG_AVG], refStats[0][AVG_ERR], refStats[0][AVG_ACOR]);
   printf("reference overlap average: % 22.15e  error: %12.5e  acor: % 7.5f\n", refStats[1][AVG_AVG], refStats[1][AVG_ERR], refStats[1][AVG_ACOR]);
-  printf("target ratio:              % 22.15e  error: %12.5e   cor: % 7.5f\n", targetRatioStats[0][AVG_AVG], targetRatioStats[0][AVG_ERR], targetBCStats[0][1]);
+  printf("target ratio:              % 22.15e  error: %12.5e   cor: % 7.5f\n", targetRatioStats[0][AVG_AVG], targetRatioStats[0][AVG_ERR], targetBCStats[0][numTargets-1]);
   printf("target average:            % 22.15e  error: %12.5e  acor: % 7.5f\n", targetStats[0][AVG_AVG], targetStats[0][AVG_ERR], targetStats[0][AVG_ACOR]);
   printf("target overlap average:    % 22.15e  error: %12.5e  acor: % 7.5f\n", targetStats[numTargets-1][AVG_AVG], targetStats[numTargets-1][AVG_ERR], targetStats[numTargets-1][AVG_ACOR]);
   for (int i=1; i<numTargets-1; i++) {
     char name[28];
-    if (targetNames && targetNames[i]) snprintf(name, 27, "%s average:", targetNames[i]);
+    if (targetNames && strlen(targetNames[i]) > 11) {
+      fprintf(stderr, "truncating name %s to 11 characters\n", targetNames[i]);
+    }
+    if (targetNames && targetNames[i]) snprintf(name, 27, "%.11s average:", targetNames[i]);
     else snprintf(name, 27, "extra %d average:", i);
     printf("%-26s % 22.15e  error: %12.5e  acor: % 7.5f\n", name, targetStats[i][AVG_AVG], targetStats[i][AVG_ERR], targetStats[i][AVG_ACOR]);
-    if (targetNames && targetNames[i]) snprintf(name, 27, "full %s average:", targetNames[i]);
+    if (targetNames && targetNames[i]) snprintf(name, 27, "%.11s ratio average:", targetNames[i]);
+    else snprintf(name, 27, "extra %d ratio average:", i);
+    printf("%-26s % 22.15e  error: %12.5e   cor: % 7.5f\n", name, targetRatioStats[i][AVG_AVG], targetRatioStats[i][AVG_ERR], targetBCStats[i][numTargets-1]);
+    if (targetNames && targetNames[i]) snprintf(name, 27, "full %.11s average:", targetNames[i]);
     else snprintf(name, 27, "full extra %d average:", i);
     printf("%-26s % 22.15e  error: %12.5e\n", name, fullStats[i][0], fullStats[i][1]);
+  }
+  if (numTargets == 1) return;
+  double** cij = targetAverage.getBlockCorrelation();
+  printf("Target Correlation:\n");
+  for (int i=0; i<numTargets-1; i++) {
+    for (int j=0; j<numTargets-1; j++) {
+      printf(" % 8.5f", cij[i][j]);
+    }
+    printf("\n");
+  }
+  cij = targetAverage.getRatioCorrelation();
+  double vd = refRatioStats[0][AVG_AVG];
+  double ed = refRatioStats[0][AVG_ERR];
+  printf("Full Correlation:\n");
+  for (int i=0; i<numTargets-1; i++) {
+    double vi = targetRatioStats[i][AVG_AVG];
+    double ei = targetRatioStats[i][AVG_ERR];
+    for (int j=0; j<numTargets-1; j++) {
+      if (j==i) {
+        printf(" % 8.5f", 1.0);
+        continue;
+      }
+      double vj = targetRatioStats[j][AVG_AVG];
+      double ej = targetRatioStats[j][AVG_ERR];
+      double c = AverageRatio::ratioCor(vi, vj, vd, ei, ej, ed, cij[i][j], 0, 0);
+      printf(" % 8.5f", c);
+    }
+    printf("\n");
   }
 }
 
