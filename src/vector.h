b4b4d2e5218c19fd2d245fb71c1e1ba6a5a88e93
Andrew Schultz <ajs42@buffalo.edu>
2018-04-16 17:18:57 -0400

    Unbreak cross product

--- src/vector.h
+++ src/vector.h
@@ -8,6 +8,6 @@ class Vector {
     static void cross(const double *v1, const double *v2, double *out) {
       out[0] = v1[1]*v2[2] - v1[2]*v2[1];
       out[1] = v1[2]*v2[0] - v1[0]*v2[2];
-      out[2] = v1[0]*v1[1] - v1[1]*v2[0];
+      out[2] = v1[0]*v2[1] - v1[1]*v2[0];
     }
 };
