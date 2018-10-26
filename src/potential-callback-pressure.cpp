680bc479b9c434e120844b4d75ab6ee258e5a736
Navneeth <navneeth@buffalo.edu>
2018-10-26 14:55:39 -0400

    changed NumAtom to NumMolecules to fix molecular pressure.

--- src/potential-callback-pressure.cpp
+++ src/potential-callback-pressure.cpp
@@ -16,9 +16,9 @@ int PotentialCallbackPressure::getNumData() {return 1;}
 void PotentialCallbackPressure::allComputeFinished(double uTot, double virialTot, double** f) {
   const double* bs = box.getBoxSize();
   double vol = bs[0]*bs[1]*bs[2];
-  int numAtoms = box.getNumAtoms();
+  int numMolecules = box.getTotalNumMolecules();
   
-  data[0] = (numAtoms*temperature - virialTot/3)/vol;
+  data[0] = (numMolecules*temperature - virialTot/3)/vol;
 }
 
 double* PotentialCallbackPressure::getData() {
